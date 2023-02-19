b28d04bdf0dc879e5325b6a409a4fdcafe6d2ef6
NexyusNex <shootdietale@gmail.com>
2023-02-19 16:19:05 +0100

    Removed import image line

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
-import clarkImg from "../images/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg";
 import "../style/header.css";
 
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <h1>Clothes shop</h1>
+    </div>
+  );
 }
