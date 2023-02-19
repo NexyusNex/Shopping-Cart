ff7c37a1ba931b47d43dbb476fcab9301c70f138
NexyusNex <shootdietale@gmail.com>
2023-02-19 16:58:14 +0100

    Added a background image for home content

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,13 @@
 import "../style/home.css";
+import clarkImg from "../images/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg";
 
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div
+        className="content"
+        style={{ backgroundImage: `url(${clarkImg})` }}
+      ></div>
+    </div>
+  );
 }
