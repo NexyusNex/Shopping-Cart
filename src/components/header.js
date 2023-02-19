5d10b4cb6a9d9e04f92846a6d840020e8df46679
NexyusNex <shootdietale@gmail.com>
2023-02-19 15:37:19 +0100

    Added styling for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,6 @@
+import clarkImg from "../images/clark-street-mercantile-P3pI6xzovu0-unsplash.jpg";
+import "../style/header.css";
+
 export default function Header() {
-  return <div></div>;
+  return <div className="header"></div>;
 }
