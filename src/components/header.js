a6a529dbcf3e083a6eb5c04f5d98012e1b7722b4
NexyusNex <shootdietale@gmail.com>
2023-02-19 17:24:21 +0100

    Added a list

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,11 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Clothes shop</h1>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+      </ul>
     </div>
   );
 }
