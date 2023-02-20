8138f86f81a7c03283a9f25d8330e06313fb3757
NexyusNex <shootdietale@gmail.com>
2023-02-20 14:10:45 +0100

    Added a path for the shop component

--- src/components/routerSwitch.js
+++ src/components/routerSwitch.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
+import Shop from "./shop";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </BrowserRouter>
   );
