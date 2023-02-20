ac8a1669dca6f407c705abf0b5383ba7ce83116a
NexyusNex <shootdietale@gmail.com>
2023-02-20 13:07:01 +0100

    Added shopping cart icon

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import "../style/header.css";
+import shoppingCart from "../images/shopping-cart.svg";
 
 export default function Header() {
   return (
     <div className="header">
       <h1>Clothes shop</h1>
-      <ul>
-        <li>Home</li>
-        <li>Shop</li>
-        <li>About</li>
-      </ul>
+      <div className="header-right">
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About</li>
+        </ul>
+        <img src={shoppingCart} alt="shopping-cart"></img>
+      </div>
     </div>
   );
 }
