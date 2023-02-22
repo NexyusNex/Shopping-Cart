76ee102245626a4b8abe1eaed6da4f8cef9d8263
NexyusNex <shootdietale@gmail.com>
2023-02-22 17:51:37 +0100

    Created the payout component

---
+++ src/components/payout.js
@@ -0,0 +1,10 @@
+import Header from "./header";
+
+export default function Payout(props) {
+  return (
+    <div className="Payout">
+      <Header cart={props.cart}></Header>
+      payout
+    </div>
+  );
+}
