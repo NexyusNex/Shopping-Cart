c7e4692a59b7755e6a8acc8d5e3d2926754e5b8c
NexyusNex <shootdietale@gmail.com>
2023-02-22 15:45:31 +0100

    Added props as state to cart

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
 import "../style/header.css";
 import shoppingCart from "../images/shopping-cart.svg";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header(props) {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    setCart((cart) => [props.cart]);
+  }, [props]);
+
   function showCart() {
     console.log(props.cart);
   }
@@ -21,6 +28,7 @@ export default function Header(props) {
           <li>About</li>
         </ul>
         <img src={shoppingCart} alt="shopping-cart" onClick={showCart}></img>
+        <div className="item-num">{cart.length}</div>
       </div>
     </div>
   );
