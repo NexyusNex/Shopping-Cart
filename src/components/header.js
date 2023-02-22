6a33c29ce3e6670be1f8416f0ac4f08cfaccc36c
NexyusNex <shootdietale@gmail.com>
2023-02-22 20:31:23 +0100

    Cart now displays the correct count

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,6 @@ import shoppingCart from "../images/shopping-cart.svg";
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
-  function showCart() {
-    console.log(props.cart);
-  }
-
   return (
     <div className="header">
       <h1>Clothes shop</h1>
@@ -21,10 +17,12 @@ export default function Header(props) {
           <li>About</li>
         </ul>
         <Link to="/Payout">
-          <img src={shoppingCart} alt="shopping-cart" onClick={showCart}></img>
+          <img src={shoppingCart} alt="shopping-cart"></img>
         </Link>
 
-        <div className="item-num">{props.cart.length}</div>
+        <div className="item-num">
+          {props.cart.reduce((total, item) => (total = total + item.count), 0)}
+        </div>
       </div>
     </div>
   );
