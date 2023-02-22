7818d8995e48e25de22312ff200f5f3f0a7320d3
NexyusNex <shootdietale@gmail.com>
2023-02-22 17:29:05 +0100

    Added a state for cart items

--- src/components/routerSwitch.js
+++ src/components/routerSwitch.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Shop from "./shop";
 
 const RouteSwitch = () => {
+  const [cart, setCart] = useState([]);
   return (
     <BrowserRouter basename="/shopping-cart">
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/" element={<App cart={cart} setCart={setCart} />} />
+        <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
       </Routes>
     </BrowserRouter>
   );
