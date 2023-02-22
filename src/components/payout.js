142c9e12aef994df6477d3f67a40e3d7d659a051
NexyusNex <shootdietale@gmail.com>
2023-02-22 18:19:34 +0100

    Added a cart container style

--- src/components/payout.js
+++ src/components/payout.js
@@ -1,10 +1,25 @@
 import Header from "./header";
+import "../style/payout.css";
+import blackFrontSweater from "../images/clothes/black-front-sweater.jpg";
+import blackTshirt from "../images/clothes/Isolated_black_t-shirt_front.png";
+import whiteTshirt from "../images/clothes/Isolated_white_t-shirt_front.png";
 
 export default function Payout(props) {
   return (
     <div className="Payout">
       <Header cart={props.cart}></Header>
-      payout
+      <div className="cart-container">
+        <h2>My cart({props.cart.length})</h2>
+        <div className="cart-item-container"></div>
+        {props.cart.map((item) => {
+          return (
+            <div className="buy-item" key={item.id}>
+              <img src={item.img} alt={item.name}></img>
+              {item.name}, ${item.cost}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
