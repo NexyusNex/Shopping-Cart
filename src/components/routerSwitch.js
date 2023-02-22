f4de2ff7418bc7b602438f9391a552d57ec58be1
NexyusNex <shootdietale@gmail.com>
2023-02-22 17:51:05 +0100

    Added a route to payout

--- src/components/routerSwitch.js
+++ src/components/routerSwitch.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Shop from "./shop";
+import Payout from "./payout";
 
 const RouteSwitch = () => {
   const [cart, setCart] = useState([]);
@@ -10,6 +11,10 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<App cart={cart} setCart={setCart} />} />
         <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+        <Route
+          path="/Payout"
+          element={<Payout cart={cart} setCart={setCart} />}
+        />
       </Routes>
     </BrowserRouter>
   );
