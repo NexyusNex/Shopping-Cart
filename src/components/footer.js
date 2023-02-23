bdffa03ce2ef9b11ffe6641525d016a83e983bb9
NexyusNex <shootdietale@gmail.com>
2023-02-23 18:20:38 +0100

    Added an input section

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,11 @@ export default function Footer() {
     <div className="Footer">
       <div className="subscribe-box">
         <h2>Subscribe to our newsletter!</h2>
+        <p>Enter your email and you'll get access to exclusive sales!</p>
+        <div className="subscribe-input">
+          <input type="email" name="email"></input>
+          <button>Subscribe</button>
+        </div>
       </div>
     </div>
   );
