99a216f1f695f044ab66dad64b41f9955fc576dc
NexyusNex <shootdietale@gmail.com>
2023-02-23 17:53:43 +0100

    Created Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="subscribe-box">
+        <h2>Subscribe to our newsletter!</h2>
+      </div>
+    </div>
+  );
+}
