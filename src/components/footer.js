6414062f887e0d3168deffd8daaef62280d5b607
NexyusNex <shootdietale@gmail.com>
2023-02-23 18:39:28 +0100

    Added sections for links

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,30 @@ export default function Footer() {
           <button>Subscribe</button>
         </div>
       </div>
+      <div className="subscribe-linkbox">
+        <h3>About us</h3>
+        <div className="links">
+          <p>FAQ</p>
+          <p>Our location</p>
+          <p>History</p>
+        </div>
+      </div>
+      <div className="subscribe-linkbox">
+        <h3>Contact</h3>
+        <div className="links">
+          <p>Email</p>
+          <p>Phone: +3812693546</p>
+          <p>Github</p>
+        </div>
+      </div>
+      <div className="subscribe-linkbox">
+        <h3>Our brands</h3>
+        <div className="links">
+          <p>Nike</p>
+          <p>Avans</p>
+          <p>Sequile</p>
+        </div>
+      </div>
     </div>
   );
 }
