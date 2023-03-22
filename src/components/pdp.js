44aae4f1d826b8484401f21f1aa77fe7cb556ee2
NexyusNex <shootdietale@gmail.com>
2023-03-22 15:07:57 +0100

    Added a container for the product

--- src/components/pdp.js
+++ src/components/pdp.js
@@ -1,10 +1,15 @@
 import Footer from "./footer";
 import Header from "./header";
+import "../style/pdp.css";
 
 export default function Pdp(props) {
   return (
     <div className="Pdp">
       <Header cart={props.cart}></Header>
+      <div className="product-container">
+        <div className="img"></div>
+        <div className="product-info"></div>
+      </div>
       <Footer></Footer>
     </div>
   );
