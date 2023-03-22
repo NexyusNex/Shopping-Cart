ea7faca6e3ad6c2b7167c06366df65036bcb9741
NexyusNex <shootdietale@gmail.com>
2023-03-22 15:30:55 +0100

    Added product dummy text

--- src/components/pdp.js
+++ src/components/pdp.js
@@ -11,6 +11,18 @@ export default function Pdp(props) {
         <div className="product-info">
           <h1>Product name</h1>
           <p className="cost-text">$22.99</p>
+          <p className="product-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            auctor gravida orci, sit amet bibendum mauris suscipit semper.
+            Curabitur bibendum convallis tellus non porta. Nunc bibendum
+            interdum justo. Cras facilisis est cursus risus cursus scelerisque.
+            Duis convallis felis vel augue dictum viverra in ac ipsum. Nunc ac
+            consequat nulla, et consectetur est. Pellentesque ut dapibus tortor.
+            Mauris vel placerat arcu, nec condimentum mauris. In fringilla
+            turpis augue, eu feugiat neque bibendum eget. Suspendisse potenti.
+            Praesent id congue urna. Curabitur mattis lorem eu efficitur
+            porttitor. Mauris at laoreet quam.
+          </p>
         </div>
       </div>
       <Footer></Footer>
