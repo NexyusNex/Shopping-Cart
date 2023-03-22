2cf1262481dd9a34f40c16086856219f6c3848db
NexyusNex <shootdietale@gmail.com>
2023-03-22 14:46:53 +0100

    Added a link for pdp page

--- src/components/routerSwitch.js
+++ src/components/routerSwitch.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Shop from "./shop";
 import Payout from "./payout";
+import Pdp from "./pdp";
 
 const RouteSwitch = () => {
   const [cart, setCart] = useState([]);
@@ -11,6 +12,7 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<App cart={cart} setCart={setCart} />} />
         <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+        <Route path="/pdp" element={<Pdp cart={cart} setCart={setCart} />} />
         <Route
           path="/Payout"
           element={<Payout cart={cart} setCart={setCart} />}
