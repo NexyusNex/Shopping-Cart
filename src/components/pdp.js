6be844461d480303649b650fdd6b18148a8f899c
NexyusNex <shootdietale@gmail.com>
2023-03-22 15:25:30 +0100

    Added title and cost

--- src/components/pdp.js
+++ src/components/pdp.js
@@ -8,7 +8,10 @@ export default function Pdp(props) {
       <Header cart={props.cart}></Header>
       <div className="product-container">
         <div className="img"></div>
-        <div className="product-info"></div>
+        <div className="product-info">
+          <h1>Product name</h1>
+          <p className="cost-text">$22.99</p>
+        </div>
       </div>
       <Footer></Footer>
     </div>
