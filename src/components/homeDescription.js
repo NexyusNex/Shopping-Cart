6f8aa729e066cf937f36025644d8b1c3ec795b83
NexyusNex <shootdietale@gmail.com>
2023-04-04 17:28:55 +0200

    Added the right section

--- src/components/homeDescription.js
+++ src/components/homeDescription.js
@@ -1,10 +1,12 @@
 import "../style/homedescription.css";
 import fashionLeft from "../images/home/freestocks-_3Q3tsJ01nc-unsplash.jpg";
+import fashionRight from "../images/home/tyler-nix-Zhh3WM2ko3s-unsplash.jpg";
 
 export default function HomeDescription() {
   return (
     <div className="home-description">
       <div className="home-description-left">
+        <img src={fashionLeft} alt="fashion img left"></img>
         <div className="home-description-left-text">
           <h1>What we do</h1>
           <p>
@@ -12,9 +14,14 @@ export default function HomeDescription() {
             and design.
           </p>
         </div>
-        <img src={fashionLeft} alt="fashion img left"></img>
       </div>
-      <div className="home-description-right"></div>
+      <div className="home-description-right">
+        <div className="home-description-right-text">
+          <h1>Why we do it</h1>
+          <p>Because, like you, we love fashion!</p>
+        </div>
+        <img src={fashionRight} alt="fashion img right"></img>
+      </div>
     </div>
   );
 }
