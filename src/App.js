2fd319772e1273cfbcf3938f53daee66c7cecde7
NexyusNex <shootdietale@gmail.com>
2023-04-04 17:00:48 +0200

    Added home description component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Home from "./components/home";
+import HomeDescription from "./components/homeDescription";
 
 function App(props) {
   return (
     <div className="App">
       <Header cart={props.cart}></Header>
       <Home></Home>
+      <HomeDescription></HomeDescription>
       <Footer></Footer>
     </div>
   );
