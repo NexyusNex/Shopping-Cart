b256c8f57dbfa33a71bd2426b238d4caddb8ac81
NexyusNex <shootdietale@gmail.com>
2023-04-04 18:10:10 +0200

    Added an observer for animation

--- src/components/homeDescription.js
+++ src/components/homeDescription.js
@@ -1,13 +1,34 @@
 import "../style/homedescription.css";
 import fashionLeft from "../images/home/freestocks-_3Q3tsJ01nc-unsplash.jpg";
 import fashionRight from "../images/home/tyler-nix-Zhh3WM2ko3s-unsplash.jpg";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function HomeDescription() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add("desc-show");
+        } else {
+          entry.target.classList.remove("desc-show");
+        }
+      });
+    });
+    const hidden = document.querySelectorAll(".desc-hidden");
+    hidden.forEach((el) => observer.observe(el));
+  }, []);
+
   return (
     <div className="home-description">
-      <div className="home-description-left">
+      <div className="home-description-left desc-hidden">
         <div className="home-desc-hline-left"></div>
-        <img src={fashionLeft} alt="fashion img left"></img>
+        <img
+          src={fashionLeft}
+          alt="fashion img left"
+          className="desc-hidden"
+        ></img>
         <div className="home-description-left-text">
           <h1>What we do</h1>
           <p>
@@ -16,12 +37,16 @@ export default function HomeDescription() {
           </p>
         </div>
       </div>
-      <div className="home-description-right">
+      <div className="home-description-right desc-hidden">
         <div className="home-description-right-text">
           <h1>Why we do it</h1>
           <p>Because, like you, we love fashion!</p>
         </div>
-        <img src={fashionRight} alt="fashion img right"></img>
+        <img
+          src={fashionRight}
+          alt="fashion img right"
+          className="desc-hidden"
+        ></img>
         <div className="home-desc-hline-right"></div>
       </div>
     </div>
