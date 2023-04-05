2cabb7df805d0fa72d08afcf9e6578133620abee
NexyusNex <shootdietale@gmail.com>
2023-04-05 13:28:50 +0200

    Added image to content div

--- src/components/homeShirt.js
+++ src/components/homeShirt.js
@@ -1,10 +1,16 @@
 import "../style/homeshirt.css";
+import shirtImg from "../images/home/luis-quintero-3qqiMT2LdR8-unsplash.jpg";
 
 export default function HomeShirt() {
   return (
     <div className="home-shirt">
-      <img></img>
-      <div className="home-shirt-content"></div>
+      <div className="home-shirt-content">
+        <img
+          src={shirtImg}
+          alt="Tshirt showcase"
+          className="home-shirt-img"
+        ></img>
+      </div>
     </div>
   );
 }
