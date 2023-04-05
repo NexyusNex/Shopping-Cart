34421d3c3ea8e63aabc73e45ecfe1bcdcdab583d
NexyusNex <shootdietale@gmail.com>
2023-04-05 13:22:22 +0200

    Initial creation of component

---
+++ src/components/homeShirt.js
@@ -0,0 +1,10 @@
+import "../style/homeshirt.css";
+
+export default function HomeShirt() {
+  return (
+    <div className="home-shirt">
+      <img></img>
+      <div className="home-shirt-content"></div>
+    </div>
+  );
+}
