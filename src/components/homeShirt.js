185f60e0186f68c70fb830fcc18ce2fcd9aa61b5
NexyusNex <shootdietale@gmail.com>
2023-04-06 14:16:37 +0200

    Added observers for scroll animation

--- src/components/homeShirt.js
+++ src/components/homeShirt.js
@@ -2,17 +2,32 @@ import "../style/homeshirt.css";
 import shirtImg from "../images/home/ivana-cajina-_7LbC5J-jw4-unsplash.jpg";
 import shirtItemImg from "../images/clothes/Isolated_black_t-shirt_front.png";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function HomeShirt() {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("hs-show");
+        } else {
+          entry.target.classList.remove("hs-show");
+        }
+      });
+    });
+    const hidden = document.querySelectorAll(".hs-hidden");
+    hidden.forEach((el) => observer.observe(el));
+  });
+
   return (
     <div className="home-shirt">
       <div className="home-shirt-content">
         <img
           src={shirtImg}
           alt="Tshirt showcase img"
-          className="home-shirt-img"
+          className="home-shirt-img hs-hidden"
         ></img>
-        <div className="home-shirt-content-text">
+        <div className="home-shirt-content-text hs-hidden">
           <h1>Get comfortable!</h1>
           <p>
             Our amazing black T-shirt made from the finest soft cotton is going
@@ -25,7 +40,7 @@ export default function HomeShirt() {
         <img
           src={shirtItemImg}
           alt="Tshirt item img"
-          className="home-shirt-item-img"
+          className="home-shirt-item-img hs-hidden"
         ></img>
       </div>
     </div>
