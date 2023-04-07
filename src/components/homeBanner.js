445891cf870ed14657d2ead5fef9fe89c107d13c
NexyusNex <shootdietale@gmail.com>
2023-04-07 19:24:40 +0200

    Added a container with items

--- src/components/homeBanner.js
+++ src/components/homeBanner.js
@@ -1,5 +1,23 @@
 import "../style/homebanner.css";
+import catTshirt from "../images/clothes/neko-front-shirt.png";
+import blueSkirt from "../images/clothes/blue-skirt.png";
+import purpleDress from "../images/clothes/purple-female-dress.png";
 
 export default function HomeBanner() {
-  return <div></div>;
+  return (
+    <div className="home-banner">
+      <h1>We also have:</h1>
+      <div className="banner-container">
+        <div className="banner-item">
+          <img
+            src={catTshirt}
+            alt="cat tshirt item"
+            className="banner-img"
+          ></img>
+        </div>
+        <div className="banner-item">sdsd</div>
+        <div className="banner-item">sdsd</div>
+      </div>
+    </div>
+  );
 }
