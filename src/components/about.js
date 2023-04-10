b210413a7ec2b883e4b05c3e8788bbf33206a211
NexyusNex <shootdietale@gmail.com>
2023-04-10 14:43:52 +0200

    Initial creation of the component

--- src/components/about.js
+++ src/components/about.js
@@ -1,3 +1,12 @@
 import "../style/about.css";
+import Footer from "./footer";
+import Header from "./header";
 
-export default function About() {}
+export default function About(props) {
+  return (
+    <div className="about">
+      <Header cart={props.cart}></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
