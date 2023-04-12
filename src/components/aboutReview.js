0457f5cf1e55835c2425c21592b8ba1f17573899
NexyusNex <shootdietale@gmail.com>
2023-04-12 19:44:02 +0200

    Initial creation

---
+++ src/components/aboutReview.js
@@ -0,0 +1,13 @@
+import "../style/aboutreview.css";
+
+export default function AboutReview() {
+  return (
+    <div className="about-review">
+      <div className="about-review-title">
+        <h1>
+          Customer <span>review</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
