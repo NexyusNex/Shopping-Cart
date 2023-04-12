8043be8be5e8ff1264e9a6ecae5205f3b379f5ba
NexyusNex <shootdietale@gmail.com>
2023-04-12 15:57:07 +0200

    Added text

--- src/components/about.js
+++ src/components/about.js
@@ -6,6 +6,18 @@ export default function About(props) {
   return (
     <div className="about">
       <Header cart={props.cart}></Header>
+      <div className="about-container">
+        <div className="about-introduction">
+          <h4>OUR MISSION</h4>
+          <h1>Making fashion available to everyone</h1>
+          <p>
+            We are a simple fashion company that ensures high quality clothes
+            for everyone, from kids to adults. Our brands are well known and our
+            costs cheap. Even if you dont follow fashion, you'll want to after
+            you visit us.
+          </p>
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
